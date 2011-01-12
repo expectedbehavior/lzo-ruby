6dd00e1b70c678d30de80425045753bd4104fc16
winebarrel <sgwr_dts@yahoo.co.jp>
2011-01-12 23:13:00 +0900

    const VERSION 0.1.2 -> 0.1.3

--- ext/lzoruby.c
+++ ext/lzoruby.c
@@ -21,7 +21,7 @@ __declspec(dllexport) void Init_lzoruby(void);
 #define RSTRING_LEN(s) (RSTRING(s)->len)
 #endif
 
-#define VERSION "0.1.2"
+#define VERSION "0.1.3"
 
 static VALUE LZO;
 static VALUE LZO_Error;
